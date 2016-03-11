e3db1ecad7523ad883183a61ff87ebea0b79befc
CodeMeow <codemeow@yahoo.com>
2016-03-11 10:25:15 +0800

    Update Models

--- console/admin.py
+++ console/admin.py
@@ -4,34 +4,44 @@ from django.contrib import admin
 
 from .models import Batch
 from .models import Commodity
+from .models import CommodityImage
+from .models import CommodityInBatch
 from .models import Customer
 from .models import Distributer
 from .models import Leader
 from .models import Order
 from .models import Membership_Order_To_Commodities
 
-class CommodityInLine(admin.StackedInline):
-	model = Commodity
+class CommodityImageInline(admin.TabularInline):
+	model = CommodityImage
+	extra = 1
+
+class CommodityAdmin(admin.ModelAdmin):
+	inlines = [CommodityImageInline]
+
+class CommodityInBatchInline(admin.TabularInline):
+	model = CommodityInBatch
 	extra = 1
 
 class BatchAdmin(admin.ModelAdmin):
-	fields = ['title', 'desc', 'leader', \
-			'end_time', 'status']
-	inlines = [CommodityInLine]
+	#fields = ['title', 'desc', 'leader', \
+	#		'end_time', 'status']
+	inlines = [CommodityInBatchInline]
 	list_display = ('title', 'desc', 'leader', \
 			'start_time', 'end_time', 'status')
 	list_filter = ['start_time']
 	search_fields = ['title']
 
-class Membership_Order_To_CommoditiesInLine(admin.TabularInline):
+class Membership_Order_To_CommoditiesInline(admin.TabularInline):
 	model = Membership_Order_To_Commodities
 	extra = 1
 
 class OrderAdmin(admin.ModelAdmin):
 	#model = Order
 	#filter_horizontal = ('commodities',)
-	inlines = (Membership_Order_To_CommoditiesInLine,)
+	inlines = (Membership_Order_To_CommoditiesInline,)
 
+admin.site.register(Commodity, CommodityAdmin)
 admin.site.register(Batch, BatchAdmin)
 admin.site.register(Customer)
 admin.site.register(Distributer)
--- console/models.py
+++ console/models.py
@@ -21,6 +21,7 @@ class Batch(models.Model):
 	title = models.CharField(max_length=200)
 	desc = models.CharField(max_length=255)
 	leader = models.ForeignKey(Leader)
+	commodities = models.ManyToManyField('Commodity', through='CommodityInBatch')
 	start_time = models.DateTimeField(auto_now=True)
 	end_time = models.DateTimeField()
 	status = models.IntegerField(max_length=10)
@@ -28,16 +29,25 @@ class Batch(models.Model):
 		return self.title
 
 class Commodity(models.Model):
-	batch = models.ForeignKey(Batch)
 	title = models.CharField(max_length=200)
 	desc = models.CharField(max_length=255)
 	details = models.TextField()
-	unit_price = models.DecimalField(max_digits=9, decimal_places=2)
 	spec = models.CharField(max_length=200)
 	stock = models.IntegerField(max_length=10)
-	quota = models.IntegerField(max_length=10)
 	def __str__(self):
 		return self.title
+
+class CommodityImage(models.Model):
+	commodity = models.ForeignKey(Commodity)
+	image = models.ImageField(upload_to='commodity_images')
+
+class CommodityInBatch(models.Model):
+	batch = models.ForeignKey(Batch)
+	commodity = models.ForeignKey(Commodity)
+	unit_price = models.DecimalField(max_digits=9, decimal_places=2)
+	quota = models.IntegerField(max_length=10)
+	def __str__(self):
+		return str(self.id)
 
 class Customer(models.Model):
 	nick_name = models.CharField(max_length=200)
