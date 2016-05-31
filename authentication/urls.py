c8d5e47e71e804e00c7436596e757d21a129da7b
CodeMeow5 <codemeow@yahoo.com>
2016-05-31 11:47:21 +0800

    Add user interface

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 from rest_auth.views import (
 	LoginView,
 	LogoutView,
+	UserDetailsView,
 )
 
 from .views import *
@@ -10,5 +11,6 @@ from .views import *
 urlpatterns = [
 	url(r'^login$', LoginView.as_view(), name='rest_login'),
 	url(r'^logout$', LogoutView.as_view(), name='rest_logout'),
+	url(r'^user$', UserDetailsView.as_view(), name='rest_user_details'),
 	url(r'^weixin_mp$', WeixinLogin.as_view(), name='wx_login'),
 ]
