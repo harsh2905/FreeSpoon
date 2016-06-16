bb2a60795bd47e514d7c1f2e58f41c52c7746d72
CodeMeow <codemeow@yahoo.com>
2016-06-16 18:43:57 +0800

    Fix bug about tz

--- business/fields.py
+++ business/fields.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+from django.utils.timezone import UTC
 import datetime
 from .utils import total_microseconds
 
 class TimestampField(serializers.Field):
 	def to_representation(self, value):
 		# value is utc time zone
-		epoch = datetime.datetime(1970, 1, 1)
+		epoch = datetime.datetime(1970, 1, 1, tzinfo=UTC())
 		#return int((value - epoch).total_seconds())
+		#import pdb
+		#pdb.set_trace()
 		return int(total_microseconds(value - epoch))
 
 	def to_internal_value(self, value):
