ce88cdade087f42fdb3c7184b770d0aa075c74ab
CodeMeow <codemeow@yahoo.com>
2016-06-22 19:08:55 +0800

    Remove session login from weixin login interface

--- authentication/adapter.py
+++ authentication/adapter.py
@@ -5,6 +5,11 @@ from allauth.account.utils import user_username
 from allauth.account.adapter import \
 	DefaultAccountAdapter as BaseDefaultAccountAdapter
 
+from allauth.account.auth_backends import AuthenticationBackend
+from django.contrib.auth import get_backends
+from django.middleware.csrf import rotate_token
+from django.contrib.auth.signals import user_logged_in
+
 class DefaultSocialAccountAdapter(BaseDefaultSocialAccountAdapter):
 	def populate_user(self,
 			request,
@@ -15,5 +20,28 @@ class DefaultSocialAccountAdapter(BaseDefaultSocialAccountAdapter):
 		return user
 
 class DefaultAccountAdapter(BaseDefaultAccountAdapter):
+	def get_login_redirect_url(self, request):
+		return ''
+
 	def populate_username(self, request, user):
 		pass
+
+	def login(self, request, user):
+		# HACK: This is not nice. The proper Django way is to use an
+		# authentication backend
+		if not hasattr(user, 'backend'):
+			backends = get_backends()
+			for backend in backends:
+				if isinstance(backend, AuthenticationBackend):
+					# prefer our own backend
+					break
+			else:
+				# Pick one
+				backend = backends[0]
+			backend_path = '.'.join([backend.__module__,
+										backend.__class__.__name__])
+			user.backend = backend_path
+		if hasattr(request, 'user'):
+			request.user = user
+		rotate_token(request)
+		user_logged_in.send(sender=user.__class__, request=request, user=user)
